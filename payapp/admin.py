44c29b29bdbe2a81f9ad7c06d1adfe820b65f6bc
Aradhya Nagaonkar <aradhya.nagaonkar_19@sakec.ac.in>
2025-04-06 17:45:12 +0530

    New changes

--- payapp/admin.py
+++ payapp/admin.py
@@ -1,3 +1,17 @@
+import django.contrib
+
+from .models import Points, PointsTransfer, PointsRequest
 from django.contrib import admin
 
-# Register your models here.
+class points(admin.ModelAdmin):
+    list_display = ['name','points', 'points_type']
+
+class points_transfer(admin.ModelAdmin):
+    list_display = ['enter_your_username','enter_destination_username', 'enter_points_to_transfer', 'points_type']
+
+class points_request(admin.ModelAdmin):
+    list_display = ['enter_your_username','enter_destination_username', 'enter_points_to_transfer', 'transfer_status', 'points_type']
+
+admin.site.register(Points, points)
+admin.site.register(PointsTransfer, points_transfer)
+admin.site.register(PointsRequest, points_request)
--- webapps2025/settings.py
+++ webapps2025/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from django.contrib.messages import constants as messages
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,7 +29,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,8 +44,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'register',
-    'payapp',
+    'django_extensions',
+    'payapp.apps.PayappConfig',
+    'register.apps.RegisterConfig',
+    'crispy_bootstrap5',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +66,7 @@ ROOT_URLCONF = 'webapps2025.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +133,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
